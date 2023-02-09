fdf1c536d33e6cde4db028decdae20707d2a73c0
toshizo0405 <113435257+toshizo0405@users.noreply.github.com>
2023-02-09 14:57:05 +0000

    [Update2]PF作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  
   devise_for :managers
   devise_for :members
+  
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
