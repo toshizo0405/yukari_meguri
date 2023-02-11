ffd603cef0d3ea5c5cc5b42cd40a14d6a8a65c52
EC2 Default User <ec2-user@ip-172-31-15-36.ap-northeast-1.compute.internal>
2023-02-11 05:39:02 +0000

    [Update4]PF作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,27 @@
 Rails.application.routes.draw do
-  
-  devise_for :managers
-  
+
+  #管理側
+
+  devise_for :managers,skip:[:registrations,:passwords],controllers: {
+    sessions: "admin/sessions"
+  }
+
   namespace :admin do
-    
+
   end
-  
-  
-  devise_for :members
-  
+
+  #会員側
+
+  devise_for :members,skip:[:passwords],controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+
   scope module: :public do
-    
+
   end
-  
-  
-  
+
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,12 @@
+Manager.create!(
+  email: 'admin@manager',
+  password: '199204',
+  )
+
+Member.create!(
+  email: 'public@member',
+  password: '199508'
+  )
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
