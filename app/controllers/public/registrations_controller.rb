100960f0f89bcd592dccfab4ab21ad8b7ecb6857
EC2 Default User <ec2-user@ip-172-31-15-36.ap-northeast-1.compute.internal>
2023-02-13 13:05:31 +0000

    [Update8]PF作成

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Public::RegistrationsController < Devise::RegistrationsController
-  # before_action :configure_sign_up_params, only: [:create]
+   before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
@@ -59,4 +59,13 @@ class Public::RegistrationsController < Devise::RegistrationsController
   # def after_inactive_sign_up_path_for(resource)
   #   super(resource)
   # end
+  def after_sign_up_path_for(resource)
+     members_my_page_path
+  end
+
+
+  def configure_sign_up_params
+     devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name,:first_name,
+     :last_name_kana,:first_name_kana,:postal_code,:address,:telephone_number])
+  end
 end
