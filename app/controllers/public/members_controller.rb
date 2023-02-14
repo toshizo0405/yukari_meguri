c6427a203a67b643943d6ee379ccf0c05cb8a8d6
toshizo0405 <113435257+toshizo0405@users.noreply.github.com>
2023-02-14 12:56:12 +0000

    [Update10]PF作成

--- app/controllers/public/members_controller.rb
+++ app/controllers/public/members_controller.rb
@@ -3,9 +3,15 @@ class Public::MembersController < ApplicationController
   def show
     @member = current_member
   end
-  
+
   def edit
-    
+    @member_edit = current_member
+  end
+
+  def update
+    @member =current_member
+    @member.update(member_params)
+    redirect_to members_my_page_path
   end
 
   def withdraw
@@ -14,4 +20,15 @@ class Public::MembersController < ApplicationController
     reset_session
     redirect_to new_member_registration_path
   end
+
+  def unsubscribe
+    @member = current_member
+  end
+
+end
+
+private
+
+def member_params
+  params.require(:member).permit(:last_name,:first_name,:last_name_kana,:first_name_kana,:email)
 end
