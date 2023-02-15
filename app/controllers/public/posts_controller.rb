918b18f98eefd2a698d1265125c81095854a7e0d
toshizo0405 <113435257+toshizo0405@users.noreply.github.com>
2023-02-15 12:38:12 +0000

    [Update12]PF作成

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -1,6 +1,43 @@
 class Public::PostsController < ApplicationController
-  
-  
-  
-  
+
+  def index
+    @posts=Post.page(params[:pege])
+  end
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  def unsubscribe
+
+  end
+
+  def new
+    @post_input = Post.new
+  end
+
+  def create
+    @post_input = Post.new(posts_params)
+    @post_input.save
+    redirect_to posts_unsubscribe_path
+  end
+
+end
+
+private
+
+def posts_params
+  params.require(:post).permit(:title,:country,:prefectues,:area,:access,:nearby_information,:go_around,:impressions)
 end
--- app/models/member.rb
+++ app/models/member.rb
@@ -4,6 +4,9 @@ class Member < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :posts,dependent: :destroy
+
+
   def self.guest
     find_or_create_by!(email: 'guest@example.com') do |member|
       member.password = SecureRandom.urlsafe_base64
