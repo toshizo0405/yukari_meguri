2fa11cf72ec51aadce17852301e78a0735431f7f
toshizo0405 <113435257+toshizo0405@users.noreply.github.com>
2023-02-19 12:50:39 +0000

    [Update23]PF作成

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -32,27 +32,29 @@ class Public::PostsController < ApplicationController
   def unsubscribe
     @post = current_member.posts.find_by(status: 0)
     if @post.nil?
-    @post_input = Post.new(posts_params)
-    @post_input.image.attach(params[:post][:image])
-    @post_input.member_id = current_member.id
-    @tag_ids = params[:post][:tag_ids]
+      @post_input = Post.new(posts_params)
+      @post_input.image.attach(params[:post][:image])
+      @post_input.member_id = current_member.id
+      @tag_ids = params[:post][:tag_ids]
      if @post_input.save
+       flash[:notice] =  "まだ投稿されていません。投稿内容を確認し、投稿ボタンをクリックしてください"
        redirect_to  confirm_path(@post_input.id,tag_ids: @tag_ids)
      else
       render :new
      end
     else
-    if params[:post][:image]
-    @post.image.purge
-    @post.image.attach(params[:post][:image])
-    end
-    @post.member_id = current_member.id
-    @tag_ids = params[:post][:tag_ids]
-      if @post.update(posts_params)
-       redirect_to  confirm_path(@post.id,tag_ids: @tag_ids)
-     else
-      render :new
-     end
+      if params[:post][:image]
+        @post.image.purge
+        @post.image.attach(params[:post][:image])
+      end
+      @post.image.purge
+      @post.member_id = current_member.id
+      @tag_ids = params[:post][:tag_ids]
+        if @post.update(posts_params)
+         redirect_to  confirm_path(@post.id,tag_ids: @tag_ids)
+       else
+        render :new
+       end
     end
 
   end
