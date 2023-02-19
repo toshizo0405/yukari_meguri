28203e2a903804d632e9f61b0a2a8911d92f2fb9
toshizo0405 <113435257+toshizo0405@users.noreply.github.com>
2023-02-19 01:15:38 +0000

    [Update20]PF作成

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,13 @@ class Post < ApplicationRecord
   belongs_to :member
   has_many:tag_middles
   has_many:tags,through: :tag_middles, dependent: :destroy
+
+   validates :title, presence: true
+   validates :prefectues, presence: true
+   validates :area, presence: true
+   validates :access, presence: true
+   validates :nearby_information, presence: true
+   validates :go_around, presence: true
+   validates :impressions, presence: true
+
 end
