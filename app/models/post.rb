8bd56d6547f906c8f94766cd389fb36ade51d88b
toshizo0405 <113435257+toshizo0405@users.noreply.github.com>
2023-02-20 11:31:41 +0000

    [Update24]PF作成

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,8 @@ class Post < ApplicationRecord
   belongs_to :member
   has_many:tag_middles
   has_many:tags,through: :tag_middles, dependent: :destroy
+  
+  
 
    def get_image
     unless image.attached?
