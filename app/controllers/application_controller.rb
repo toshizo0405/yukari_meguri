ddc4dd4f4d590a3c8f28a8fe735874c41dc91b70
toshizo0405 <113435257+toshizo0405@users.noreply.github.com>
2023-02-25 14:12:39 +0000

    [Update33]PF作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,12 @@
 class ApplicationController < ActionController::Base
-  
+
 add_flash_types :success, :info, :warning, :danger
+
+def index
+  # 検索オブジェクト
+  @search = Post.ransack(params[:q])
+  # 検索結果
+  @posts = @search.result
+end
 
 end
