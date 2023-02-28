07f3500abe51b2306d48682518038f320a22ac6c
toshizo0405 <113435257+toshizo0405@users.noreply.github.com>
2023-02-28 11:46:19 +0000

    [Update42]PF作成

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -1,2 +1,23 @@
 class Admin::PostsController < ApplicationController
+
+  def index
+
+  end
+
+  def show
+    @post_detail = Post.find(params[:id])
+  end
+
+  def destroy
+    @post_detail = Post.find(params[:id])
+    @post_detail.destroy
+    redirect_to admin_root_path
+  end
+
+end
+
+private
+
+def posts_params
+  params.require(:post).permit(:title,:prefectues,:area,:access,:nearby_information,:go_around,:impressions,:image, tags_ids: [] )
 end
