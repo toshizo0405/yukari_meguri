7c38b3e5a3ce8e78836758f65be15ccd06e13bb7
toshizo0405 <113435257+toshizo0405@users.noreply.github.com>
2023-03-01 09:34:05 +0000

    [Update45]PF作成

--- app/controllers/admin/members_controller.rb
+++ app/controllers/admin/members_controller.rb
@@ -1,19 +1,26 @@
 class Admin::MembersController < ApplicationController
-  
+
   def index
-    
+    @members = Member.all
   end
-  
+
   def show
-    
+    @member = Member.find(params[:id])
   end
-  
+
   def edit
-    
+
   end
-  
+
   def update
-    
+
   end
-  
+
+end
+
+private
+
+def member_params
+  params.require(:member).permit(
+    )
 end
