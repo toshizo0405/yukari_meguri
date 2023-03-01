cc385b2d4212e016f2b1f4104e0d66cb349b490b
toshizo0405 <113435257+toshizo0405@users.noreply.github.com>
2023-03-01 12:36:01 +0000

    [Update48]PF作成

--- app/controllers/admin/members_controller.rb
+++ app/controllers/admin/members_controller.rb
@@ -13,7 +13,12 @@ class Admin::MembersController < ApplicationController
   end
 
   def update
-
+    @member = Member.find(params[:id])
+    if @member.update(member_params)
+      redirect_to admin_member_path(@member.id)
+    else
+      render :edit
+    end
   end
 
 end
