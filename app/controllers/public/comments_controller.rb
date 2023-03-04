a9d4982ae3dc16c377d94f54a1225241fd33a836
toshizo0405 <113435257+toshizo0405@users.noreply.github.com>
2023-03-04 07:47:14 +0000

    [Update58]PF作成

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -8,6 +8,11 @@ class Public::CommentsController < ApplicationController
     redirect_to post_path(post)
   end
 
+  def destroy
+    Comment.find(params[:id]).destroy
+    redirect_to post_path(params[:post_id])
+  end
+
   private
 
   def comment_params
