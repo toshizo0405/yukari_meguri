614bdb32d845d44aaed66dc95ec1768eda8551c4
toshizo0405 <113435257+toshizo0405@users.noreply.github.com>
2023-03-09 11:28:39 +0000

    [Update85]PF作成

--- app/models/post.rb
+++ app/models/post.rb
@@ -12,7 +12,7 @@ class Post < ApplicationRecord
 
 
 
-    def get_image
+    def get_image(width, height)
         unless image.attached?
           file_path = Rails.root.join('app/assets/images/no_image.jpg')
           image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
