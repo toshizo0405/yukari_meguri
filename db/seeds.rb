c22cba74dcf378140e0df885adf606a746cedeb7
toshizo0405 <113435257+toshizo0405@users.noreply.github.com>
2023-03-26 13:37:43 +0000

    [Finish3]PF作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,26 +1,28 @@
 Manager.create!(
-  email: 'admin@manager',
-  password: '199204',
-  )
+email: 'admin@manager',
+password: '199204',
+)
 
-#Member.create!(
-  #email: 'public@member',
-  #password: '199508'
-  #)
+Member.create!([
+  {email: 'nobunaga@member',password: '199508',last_name: '織田',first_name: '信長',last_name_kana: 'オダ',first_name_kana: 'ノブナガ',}
+  ])
 
-  Tag.create([
-  { name: '古墳時代' },
-  { name: '飛鳥時代' },
-  { name: '奈良時代' },
-  { name: '平安時代' },
-  { name: '鎌倉時代' },
-  { name: '室町時代' },
-  { name: '安土桃山時代' },
-  { name: '江戸時代' },
-  { name: '明治時代' },
-  { name: '大正時代' },
-  { name: '昭和時代' },
+Tag.create([
+{ name: '古墳時代' },
+{ name: '飛鳥時代' },
+{ name: '奈良時代' },
+{ name: '平安時代' },
+{ name: '鎌倉時代' },
+{ name: '室町時代' },
+{ name: '安土桃山時代' },
+{ name: '江戸時代' },
+{ name: '明治時代' },
+{ name: '大正時代' },
+{ name: '昭和時代' },
 ])
+
+
+
 
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
